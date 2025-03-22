bc519678c5cbf869d6a76bb412495938142de825
Luka <LPolic@its.jnj.com>
2025-03-22 09:24:56 +0100

    Add components.

---
+++ my-frontend/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to the game</h1>
+      <p>Please choose option to proceed: </p>
+
+      <button onClick={() => navigate("/login")}>Log In</button>
+      <button onClick={() => navigate("/signup")}>Sign Up</button>
+      <button onClick={() => navigate("/game")}>Continue Anonymously</button>
+    </div>
+  );
+}
+
+export default Home;
---
+++ my-frontend/src/components/StartGame.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function StartGame() {
+  return (
+    <div>
+      <h1>The Game Has Started!</h1>
+      <p>Welcome to the game screen.</p>
+    </div>
+  );
+}
+
+export default StartGame;
