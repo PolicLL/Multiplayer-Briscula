0cb10ce9f9ba21dc46277aeecd591dd784a8b05e
Luka <LPolic@its.jnj.com>
2025-04-14 14:11:05 +0200

    Temp.

--- briscula/src/main/java/com/example/web/controller/GameController.java
+++ briscula/src/main/java/com/example/web/controller/GameController.java
@@ -1,9 +1,14 @@
 package com.example.web.controller;
 
+import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/game")
 public class GameController {
 
-  @PostMapping("/start-game/{player-id}")
-  public void startGame(@PathVariable("player-id") String playerId) {
+  @PostMapping("/join")
+  public ResponseEntity<Map<String, String>> joinGame(@RequestBody JoinGameRequest request) {
+    String playerId = lobbyService.addPlayer(request.getPlayerName());
+    return ResponseEntity.ok(Map.of("playerId", playerId));
+  }
+
+  @GetMapping("/status/{playerId}")
+  public ResponseEntity<Map<String, String>> checkGameStatus(@PathVariable String playerId) {
+    Optional<String> roomIdOpt = lobbyService.getAssignedRoomId(playerId);
+
+    return roomIdOpt.map(string -> ResponseEntity.ok(Map.of(
+        "status", "READY",
+        "roomId", string
+    ))).orElseGet(() -> ResponseEntity.ok(Map.of("status", "WAITING")));
 
   }
+
 
 }
