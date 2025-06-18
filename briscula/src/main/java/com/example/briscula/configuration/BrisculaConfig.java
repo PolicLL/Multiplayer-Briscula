451a19844936b240293b90a66a67c3b4d96dbb53
Luka <LPolic@its.jnj.com>
2025-06-18 18:37:34 +0200

    Updating configuration.

--- briscula/src/main/java/com/example/briscula/configuration/BrisculaConfig.java
+++ briscula/src/main/java/com/example/briscula/configuration/BrisculaConfig.java
@@ -1,16 +1,24 @@
 package com.example.briscula.configuration;
 
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "briscula")
+@Getter
+@Setter
 public class BrisculaConfig {
 
-  private static int waitingTimeInSecondsForChoosingCard;
+  private int waitingTimeInSecondsForChoosingCard;
 
-  public static int getWaitingTimeStatic() {
-    return waitingTimeInSecondsForChoosingCard;
+  @Getter
+  private static int waitingTimeStatic;
+
+  @PostConstruct
+  public void init() {
+    waitingTimeStatic = waitingTimeInSecondsForChoosingCard;
   }
 }
-
